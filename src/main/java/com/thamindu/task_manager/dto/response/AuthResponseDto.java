bb60c282ca4eea2c55eea18ef3fa7d2c5a9d807e
Thamindu Weeravardana <thaminduweeravardana@gmail.com>
2026-08-06 18:59:53 +0530

    feat(auth): create AuthResponseDto record for authentication payload

--- src/main/java/com/thamindu/task_manager/dto/response/AuthResponseDto.java
+++ src/main/java/com/thamindu/task_manager/dto/response/AuthResponseDto.java
@@ -1,4 +1,14 @@
 package com.thamindu.task_manager.dto.response;
 
-public record AuthResponseDto() {
+import lombok.Builder;
+
+import java.util.Set;
+
+@Builder
+public record AuthResponseDto(
+        String token,
+        String tokenType,
+        String username,
+        Set<String> roles
+) {
 }
