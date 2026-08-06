a3710dbed57221c2ace3463db844bb39bac97b5c
Thamindu Weeravardana <thaminduweeravardana@gmail.com>
2026-08-06 19:21:29 +0530

    refactor(exception): update UserNotFoundException structure
    - Improve constructor flexibility and add field context attributes
    - Ensure clean exception naming to prevent framework collisions

--- src/main/java/com/thamindu/task_manager/exception/UsernameNotFoundException.java
+++ src/main/java/com/thamindu/task_manager/exception/UsernameNotFoundException.java
@@ -1,12 +1,12 @@
-package com.activity_hub.notification_feed.exception;
+package com.thamindu.task_manager.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class NotFoundException extends RuntimeException {
+public class UsernameNotFoundException extends RuntimeException {
 
-    public NotFoundException(String message) {
+    public UsernameNotFoundException(String message) {
         super(message);
     }
 
