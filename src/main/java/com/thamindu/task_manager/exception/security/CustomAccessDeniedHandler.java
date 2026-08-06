2fd4df74a936d564e013cf7e834e0c1ec4b605f3
Thamindu Weeravardana <thaminduweeravardana@gmail.com>
2026-08-06 19:04:02 +0530

    feat(security): add custom authentication entry point and access denied handler

--- src/main/java/com/thamindu/task_manager/exception/security/CustomAccessDeniedHandler.java
+++ src/main/java/com/thamindu/task_manager/exception/security/CustomAccessDeniedHandler.java
@@ -7,37 +7,35 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jspecify.annotations.NonNull;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 import tools.jackson.databind.ObjectMapper;
-import java.awt.*;
 import java.io.IOException;
 import java.time.Instant;
 
 @RequiredArgsConstructor
 @Slf4j
 @Component
-public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 
     private final ObjectMapper objectMapper;
 
     @Override
-    public void commence(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull AuthenticationException authException) throws IOException {
-        log.error("Unauthorized error occurred on path: {}. Message: {}", request.getRequestURI(), authException.getMessage());
-
+    public void handle(HttpServletRequest request, HttpServletResponse response, @NonNull AccessDeniedException accessDeniedException) throws IOException {
+        log.warn("Forbidden access attempt on path: {} by user", request.getRequestURI());
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 
-       ErrorResponseDto errorPayload =  new ErrorResponseDto(
+        ErrorResponseDto errorPayload =  new ErrorResponseDto(
                 Instant.now(),
                 HttpServletResponse.SC_UNAUTHORIZED,
-                "Unauthorized Access",
-                "You must be authenticated to access this resource",
+                "Forbidden Access",
+                "You do not have the required permissions to perform this operation.",
                 request.getRequestURI()
         );
 
-       objectMapper.writeValue(response.getOutputStream(), errorPayload);
+        objectMapper.writeValue(response.getOutputStream(), errorPayload);
     }
 }
 
