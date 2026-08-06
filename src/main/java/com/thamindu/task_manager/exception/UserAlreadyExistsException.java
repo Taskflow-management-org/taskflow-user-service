a0a6912c3b71925e8b4fad11c3c124d9bba5d76b
Thamindu Weeravardana <thaminduweeravardana@gmail.com>
2026-08-06 19:19:22 +0530

    refactor(exception): enhance UserAlreadyExistsException constructors

--- src/main/java/com/thamindu/task_manager/exception/UserAlreadyExistsException.java
+++ src/main/java/com/thamindu/task_manager/exception/UserAlreadyExistsException.java
@@ -1,4 +1,8 @@
 package com.thamindu.task_manager.exception;
 
-public class UserAlreadyExistsException {
+public class UserAlreadyExistsException extends RuntimeException {
+
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
 }
